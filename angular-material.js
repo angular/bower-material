9b315968e359f4b9a1c116abd0349b7213d3af04
Andrew <andrewtjoslin@gmail.com>
2014-08-19 13:59:32 -0600

    release: version 0.0.1

--- angular-material.js
+++ angular-material.js
@@ -990,7 +990,7 @@ angular.module('material.animations')
 /**
  * noink/nobar/nostretch directive: make any element that has one of
  * these attributes be given a controller, so that other directives can 
- * `require:` these and see if there is a `nosomething` parent.
+ * `require:` these and see if there is a `no<xxx>` parent attribute.
  *
  * @usage
  * <hljs lang="html">
@@ -1089,15 +1089,15 @@ function attrNoDirective() {
         var options  = calculateOptions(element, attrs);
         var tag =
           '<canvas ' +
-          'class="material-ink-ripple {{classList}}"' +
-          'style="top:{{top}}; left:{{left}}" >' +
+            'class="material-ink-ripple {{classList}}"' +
+            'style="top:{{top}}; left:{{left}}" >' +
           '</canvas>';
 
         element.replaceWith(
           angular.element( $interpolate(tag)(options) )
         );
 
-      return function linkCanvas( scope, element, attrs, noinkCtrl ) {
+      return function postLink( scope, element, attrs, noinkCtrl ) {
         if ( noinkCtrl ) return;
 
         var ripple, watchMouse,
@@ -1246,6 +1246,8 @@ angular.module('material.components.button', [
  * @param {boolean=} noink Flag indicates use of ripple ink effects
  * @param {boolean=} disabled Flag indicates if the tab is disabled: not selectable with no ink effects
  * @param {string=} type Optional attribute to specific button types (useful for forms); such as 'submit', etc.
+ * @param {string=} ng-ref Optional attribute to support both ARIA and link navigation
+ * @param {string=} href Optional attribute to support both ARIA and link navigation
  *
  * @usage
  * <hljs lang="html">
@@ -1268,44 +1270,33 @@ function MaterialButtonDirective(ngHrefDirectives) {
     template: '<material-ripple start="center" initial-opacity="0.25" opacity-decay-velocity="0.75"></material-ripple>',
     compile: function(element, attr) {
 
+      // Add an inner anchor if the element has a `href` or `ngHref` attribute,
+      // so this element can be clicked like a normal `<a>`.
       var href = attr.ngHref || attr.href;
+      var innerElement;
       if (href) {
-        var innerAnchor = angular.element('<a>')
-          .addClass('material-button-inner')
-          .attr('ng-href', href);
-        element.append(innerAnchor);
+        innerElement = angular.element('<a>')
+          .attr('ng-href', href)
+          .attr('rel', attr.rel)
+          .attr('target', attr.target);
+
+      // Otherwise, just add an inner button element (for form submission etc)
+      } else {
+        innerElement = angular.element('<button>')
+          .attr('type', attr.type)
+          .attr('disabled', attr.ngDisabled || attr.disabled)
+          .attr('form', attr.form);
       }
+      innerElement.addClass('material-button-inner');
+      element.append(innerElement);
 
       return function postLink(scope, element, attr, ctrl, transclude) {
-
         // Put the content of the <material-button> inside after the ripple
+        // and inner elements
         transclude(scope, function(clone) {
           element.append(clone);
         });
-
-        element.attr({
-          role: 'button',
-          tabIndex: '0'
-        });
-
-        configureButton(attr.type);
-
-        /**
-         * If a type attribute is specified, dynamically insert a <button> element.
-         * This is vital to allow <material-button> to be used as form buttons
-         * @param type
-         */
-        function configureButton(type) {
-          if (type) {
-            var innerButton = angular.element('<button>')
-              .attr('type', type)
-              .addClass('material-button-inner');
-            element.append(innerButton);
-          }
-        }
-
       };
-
     }
   };
 
@@ -1672,6 +1663,7 @@ angular.module('material.components.form', [])
   ]);
 
 /**
+ * @private
  * @ngdoc directive
  * @name materialInputGroup
  * @module material.components.form
@@ -1853,7 +1845,7 @@ angular.module('material.components.radioButton', [
  * `<material-radio-button>` tags.
  *
  * @param {string} ngModel Assignable angular expression to data-bind to.
- * @param {boolean=} noink Use of attribute indicates use of ripple ink effects on radio buttons gestures
+ * @param {boolean=} noink Use of attribute indicates flag to disable ink ripple effects.
  *
  * @usage
  * <hljs lang="html">
