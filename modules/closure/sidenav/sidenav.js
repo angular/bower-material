252d44f381cab4f381ea41491744356295c381a0
Thomas Burleson <ThomasBurleson@Gmail.com>
2014-11-19 15:30:32 -0600

    release: version 0.6.0-rc1

--- modules/closure/sidenav/sidenav.js
+++ modules/closure/sidenav/sidenav.js
@@ -2,7 +2,7 @@
  * Angular Material Design
  * https://github.com/angular/material
  * @license MIT
- * v0.5.1-master-96d3208
+ * v0.6.0-rc1
  */
 goog.provide('ng.material.components.sidenav');
 goog.require('ng.material.components.backdrop');
