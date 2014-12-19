bd757155fae91753960fa1997fc55306370fd2d6
Thomas Burleson <ThomasBurleson@Gmail.com>
2014-12-19 06:08:45 -0600

    release: version 0.7.0-rc1

--- modules/closure/bottomSheet/bottomSheet.js
+++ modules/closure/bottomSheet/bottomSheet.js
@@ -2,7 +2,7 @@
  * Angular Material Design
  * https://github.com/angular/material
  * @license MIT
- * v0.6.1-master-d1d58bc
+ * v0.7.0-rc1
  */
 goog.provide('ng.material.components.bottomSheet');
 goog.require('ng.material.components.backdrop');
--- modules/closure/tabs/tabs.js
+++ modules/closure/tabs/tabs.js
@@ -2,7 +2,7 @@
  * Angular Material Design
  * https://github.com/angular/material
  * @license MIT
- * v0.6.1-master-d1d58bc
+ * v0.7.0-rc1
  */
 goog.provide('ng.material.components.tabs');
 goog.require('ng.material.core');
