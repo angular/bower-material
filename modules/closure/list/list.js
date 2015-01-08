ca43957ac5b9393592bb96203b8cb0fc57c4fa79
Andrew <andrewtjoslin@gmail.com>
2015-01-08 09:16:34 -0700

    release: version 0.7.0-rc2

--- modules/closure/list/list.js
+++ modules/closure/list/list.js
@@ -2,7 +2,7 @@
  * Angular Material Design
  * https://github.com/angular/material
  * @license MIT
- * v0.7.0-rc1-master-bf9bd1b
+ * v0.7.0-rc2
  */
 goog.provide('ng.material.components.list');
 goog.require('ng.material.core');
