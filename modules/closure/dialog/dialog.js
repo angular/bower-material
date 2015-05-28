2b5130c1c00deaaf1918bd613f23d7832e7cb0f1
Robert Messerle <rmesserle@google.com>
2015-05-28 10:40:49 -0700

    release: version 0.9.6

--- modules/closure/dialog/dialog.js
+++ modules/closure/dialog/dialog.js
@@ -2,7 +2,7 @@
  * Angular Material Design
  * https://github.com/angular/material
  * @license MIT
- * v0.9.5-master-7a1ad41
+ * v0.9.6
  */
 goog.provide('ng.material.components.dialog');
 goog.require('ng.material.components.backdrop');
