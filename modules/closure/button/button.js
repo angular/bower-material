5f7de8ced18d4663d7a90caecf620fc1f8a66568
Robert Messerle <rmesserle@google.com>
2015-06-08 11:38:28 -0700

    release: version 0.9.8

--- modules/closure/button/button.js
+++ modules/closure/button/button.js
@@ -2,7 +2,7 @@
  * Angular Material Design
  * https://github.com/angular/material
  * @license MIT
- * v0.9.8-rc1-master-dd04192
+ * v0.9.8
  */
 goog.provide('ng.material.components.button');
 goog.require('ng.material.core');
--- modules/js/tooltip/tooltip.js
+++ modules/js/tooltip/tooltip.js
@@ -2,7 +2,7 @@
  * Angular Material Design
  * https://github.com/angular/material
  * @license MIT
- * v0.9.8-rc1-master-dd04192
+ * v0.9.8
  */
 (function( window, angular, undefined ){
 "use strict";
