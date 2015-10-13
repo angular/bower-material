2f17e27fd96c3b3aa7f278a34a199a56869ee86f
Robert Messerle <rmesserle@google.com>
2015-10-13 12:43:29 -0700

    release: version 0.11.4

--- modules/closure/fabTrigger/fabTrigger.js
+++ modules/closure/fabTrigger/fabTrigger.js
@@ -2,7 +2,7 @@
  * Angular Material Design
  * https://github.com/angular/material
  * @license MIT
- * v0.11.3-master-3fe7d76
+ * v0.11.4
  */
 goog.provide('ng.material.components.fabTrigger');
 goog.require('ng.material.core');
