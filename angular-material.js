0488d683748e7f4c1a27471858251ce9d36bc811
Michael Prentice <splaktar@gmail.com>
2018-01-10 22:45:11 +0000

    release: version 1.1.5-master-6b44ffc

--- angular-material.js
+++ angular-material.js
@@ -2,7 +2,7 @@
  * AngularJS Material Design
  * https://github.com/angular/material
  * @license MIT
- * v1.1.5-master-f3a54fc
+ * v1.1.5-master-6b44ffc
  */
 (function( window, angular, undefined ){
 "use strict";
@@ -36381,4 +36381,4 @@ angular.module("material.core").constant("$MD_THEME_CSS", "md-autocomplete.md-TH
 })();
 
 
-})(window, window.angular);;window.ngMaterial={version:{full: "1.1.5-master-f3a54fc"}};
+})(window, window.angular);;window.ngMaterial={version:{full: "1.1.5-master-6b44ffc"}};
--- modules/closure/fabToolbar/fabToolbar.js
+++ modules/closure/fabToolbar/fabToolbar.js
@@ -2,7 +2,7 @@
  * AngularJS Material Design
  * https://github.com/angular/material
  * @license MIT
- * v1.1.5-master-f3a54fc
+ * v1.1.5-master-6b44ffc
  */
 goog.provide('ngmaterial.components.fabToolbar');
 goog.require('ngmaterial.components.fabActions');
