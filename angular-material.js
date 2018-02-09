f8db1c3d60365e76ab608b356e1934dad50cc230
Carlos Lopez <clshortfuse@gmail.com>
2018-02-09 23:55:01 +0000

    release: version 1.1.7-master-0f15e39

--- angular-material.js
+++ angular-material.js
@@ -2,7 +2,7 @@
  * AngularJS Material Design
  * https://github.com/angular/material
  * @license MIT
- * v1.1.7-master-0d276f3
+ * v1.1.7-master-0f15e39
  */
 (function( window, angular, undefined ){
 "use strict";
@@ -36464,4 +36464,4 @@ angular.module("material.core").constant("$MD_THEME_CSS", "md-autocomplete.md-TH
 })();
 
 
-})(window, window.angular);;window.ngMaterial={version:{full: "1.1.7-master-0d276f3"}};
+})(window, window.angular);;window.ngMaterial={version:{full: "1.1.7-master-0f15e39"}};
