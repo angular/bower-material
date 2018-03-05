1f010d474ff7e993fa4e152029654ba9805568bc
Philippe MARTIN <contact@elol.fr>
2018-03-05 17:20:31 +0000

    release: version 1.1.7-master-af7d310

--- angular-material.js
+++ angular-material.js
@@ -2,7 +2,7 @@
  * AngularJS Material Design
  * https://github.com/angular/material
  * @license MIT
- * v1.1.7-master-7de4d36
+ * v1.1.7-master-af7d310
  */
 (function( window, angular, undefined ){
 "use strict";
@@ -36476,4 +36476,4 @@ angular.module("material.core").constant("$MD_THEME_CSS", "md-autocomplete.md-TH
 })();
 
 
-})(window, window.angular);;window.ngMaterial={version:{full: "1.1.7-master-7de4d36"}};
+})(window, window.angular);;window.ngMaterial={version:{full: "1.1.7-master-af7d310"}};
