15fa18c124607ea7a5a524cd351983b76a3e6d2d
Immanuel von Detten <i.vondetten@gmail.com>
2018-03-24 00:06:07 +0000

    release: version 1.1.8-master-1ef9ec3

--- angular-material.js
+++ angular-material.js
@@ -2,7 +2,7 @@
  * AngularJS Material Design
  * https://github.com/angular/material
  * @license MIT
- * v1.1.8-master-c62282f
+ * v1.1.8-master-1ef9ec3
  */
 (function( window, angular, undefined ){
 "use strict";
@@ -36595,4 +36595,4 @@ angular.module("material.core").constant("$MD_THEME_CSS", "md-autocomplete.md-TH
 })();
 
 
-})(window, window.angular);;window.ngMaterial={version:{full: "1.1.8-master-c62282f"}};
+})(window, window.angular);;window.ngMaterial={version:{full: "1.1.8-master-1ef9ec3"}};
