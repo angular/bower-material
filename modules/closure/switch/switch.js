e793095d2b06b4e38c89aec6159c0d277426fa2c
Andrew Seguin <andrewjs@google.com>
2018-04-27 14:32:06 -0700

    release: version 1.1.9

--- modules/closure/switch/switch.js
+++ modules/closure/switch/switch.js
@@ -2,7 +2,7 @@
  * AngularJS Material Design
  * https://github.com/angular/material
  * @license MIT
- * v1.1.8-master-27c0c81
+ * v1.1.9
  */
 goog.provide('ngmaterial.components.switch');
 goog.require('ngmaterial.components.checkbox');
