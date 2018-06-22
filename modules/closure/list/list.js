f0e2de1fa20a3f8128e9890f323c3171e7b7cc02
Michael Prentice <splaktar@gmail.com>
2018-06-22 15:11:26 +0000

    release: version 1.1.9-master-a6bca73

--- modules/closure/list/list.js
+++ modules/closure/list/list.js
@@ -2,7 +2,7 @@
  * AngularJS Material Design
  * https://github.com/angular/material
  * @license MIT
- * v1.1.9-master-5e37b63
+ * v1.1.9-master-a6bca73
  */
 goog.provide('ngmaterial.components.list');
 goog.require('ngmaterial.core');
