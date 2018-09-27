1b5705659afd18da51adb571489e353d7edf7b51
Michael Prentice <splaktar@gmail.com>
2018-09-27 01:03:36 +0000

    release: version 1.1.10-master-0671aab

--- modules/closure/sticky/sticky.js
+++ modules/closure/sticky/sticky.js
@@ -2,7 +2,7 @@
  * AngularJS Material Design
  * https://github.com/angular/material
  * @license MIT
- * v1.1.10-master-bc8d29f
+ * v1.1.10-master-0671aab
  */
 goog.provide('ngmaterial.components.sticky');
 goog.require('ngmaterial.components.content');
