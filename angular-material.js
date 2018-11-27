68f2553485a8acb93471016e178b278c3baa5657
Michael Prentice <splaktar@gmail.com>
2018-11-27 21:36:40 +0000

    release: version 1.1.10-master-5342ca2

--- angular-material.js
+++ angular-material.js
@@ -2,7 +2,7 @@
  * AngularJS Material Design
  * https://github.com/angular/material
  * @license MIT
- * v1.1.10-master-c1c4523
+ * v1.1.10-master-5342ca2
  */
 (function( window, angular, undefined ){
 "use strict";
@@ -37896,4 +37896,4 @@ angular.module("material.core").constant("$MD_THEME_CSS", "md-autocomplete.md-TH
 })();
 
 
-})(window, window.angular);;window.ngMaterial={version:{full: "1.1.10-master-c1c4523"}};
+})(window, window.angular);;window.ngMaterial={version:{full: "1.1.10-master-5342ca2"}};
