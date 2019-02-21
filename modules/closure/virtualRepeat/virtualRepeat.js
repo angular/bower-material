26714eb581ef6a9d55484ee21e1a177f0f22d567
Michael Prentice <splaktar@gmail.com>
2019-02-21 17:09:27 +0000

    release: version 1.1.13-master-d3773a5

--- modules/closure/virtualRepeat/virtualRepeat.js
+++ modules/closure/virtualRepeat/virtualRepeat.js
@@ -2,7 +2,7 @@
  * AngularJS Material Design
  * https://github.com/angular/material
  * @license MIT
- * v1.1.13-master-77f4c04
+ * v1.1.13-master-d3773a5
  */
 goog.provide('ngmaterial.components.virtualRepeat');
 goog.require('ngmaterial.components.showHide');
