4c283842a1afbe4f555c57132f65b6c2e33bb95c
Miles Malerba <mmalerba@google.com>
2019-03-18 17:22:51 +0000

    release: version 1.1.14-master-ab9e852

--- angular-material.js
+++ angular-material.js
@@ -2,7 +2,7 @@
  * AngularJS Material Design
  * https://github.com/angular/material
  * @license MIT
- * v1.1.14
+ * v1.1.14-master-ab9e852
  */
 (function( window, angular, undefined ){
 "use strict";
@@ -38345,4 +38345,4 @@ angular.module("material.core").constant("$MD_THEME_CSS", "md-autocomplete.md-TH
 })();
 
 
-})(window, window.angular);;window.ngMaterial={version:{full: "1.1.14"}};
+})(window, window.angular);;window.ngMaterial={version:{full: "1.1.14-master-ab9e852"}};
