5d90c65843e2ebe54f343062be2a8c8cad320ccb
Jeremy Elbourn <jelbourn@google.com>
2019-03-29 17:39:30 -0700

    release: version 1.1.16

--- angular-material.js
+++ angular-material.js
@@ -2,7 +2,7 @@
  * AngularJS Material Design
  * https://github.com/angular/material
  * @license MIT
- * v1.1.15-master-2dd34ab
+ * v1.1.16
  */
 (function( window, angular, undefined ){
 "use strict";
@@ -38367,4 +38367,4 @@ angular.module("material.core").constant("$MD_THEME_CSS", "md-autocomplete.md-TH
 })();
 
 
-})(window, window.angular);;window.ngMaterial={version:{full: "1.1.15-master-2dd34ab"}};
+})(window, window.angular);;window.ngMaterial={version:{full: "1.1.16"}};
