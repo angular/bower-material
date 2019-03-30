bf5f2575145b5b95c18ef875be523b97cf101f45
Jeremy Elbourn <jelbourn@google.com>
2019-03-30 00:59:00 +0000

    release: version 1.1.17-master-fe4b8c3

--- modules/closure/virtualRepeat/virtualRepeat.js
+++ modules/closure/virtualRepeat/virtualRepeat.js
@@ -2,7 +2,7 @@
  * AngularJS Material Design
  * https://github.com/angular/material
  * @license MIT
- * v1.1.16-master-db3cdf7
+ * v1.1.17-master-fe4b8c3
  */
 goog.provide('ngmaterial.components.virtualRepeat');
 goog.require('ngmaterial.components.showHide');
