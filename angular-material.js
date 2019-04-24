8769c3947f1832a145217eb90f3ff5e663b1ec50
codymikol <codymikol@gmail.com>
2019-04-24 20:19:32 +0000

    release: version 1.1.18-master-84ac558

--- angular-material.js
+++ angular-material.js
@@ -2,7 +2,7 @@
  * AngularJS Material Design
  * https://github.com/angular/material
  * @license MIT
- * v1.1.18-master-1063a92
+ * v1.1.18-master-84ac558
  */
 (function( window, angular, undefined ){
 "use strict";
@@ -38388,4 +38388,4 @@ angular.module("material.core").constant("$MD_THEME_CSS", "md-autocomplete.md-TH
 })();
 
 
-})(window, window.angular);;window.ngMaterial={version:{full: "1.1.18-master-1063a92"}};
+})(window, window.angular);;window.ngMaterial={version:{full: "1.1.18-master-84ac558"}};
