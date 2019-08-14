b9b9cd8e5df9d3ee70db5efa94a209028d435479
Michael Prentice <splaktar@gmail.com>
2019-08-14 16:17:10 +0000

    release: version 1.1.19-master-b5de054

--- modules/closure/panel/panel.js
+++ modules/closure/panel/panel.js
@@ -2,7 +2,7 @@
  * AngularJS Material Design
  * https://github.com/angular/material
  * @license MIT
- * v1.1.19-master-4acef53
+ * v1.1.19-master-b5de054
  */
 goog.provide('ngmaterial.components.panel');
 goog.require('ngmaterial.components.backdrop');
