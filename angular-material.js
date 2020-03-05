3e970f148aeac05ea4fb4b200a4f875a82d0f14a
Michael Prentice <splaktar@gmail.com>
2020-03-05 23:57:31 +0000

    release: version 1.1.21-master-0ab73bd

--- angular-material.js
+++ angular-material.js
@@ -2,7 +2,7 @@
  * AngularJS Material Design
  * https://github.com/angular/material
  * @license MIT
- * v1.1.21-master-d537d25
+ * v1.1.21-master-0ab73bd
  */
 (function( window, angular, undefined ){
 "use strict";
@@ -38617,4 +38617,4 @@ angular.module("material.core").constant("$MD_THEME_CSS", "md-autocomplete.md-TH
 })();
 
 
-})(window, window.angular);;window.ngMaterial={version:{full: "1.1.21-master-d537d25"}};
+})(window, window.angular);;window.ngMaterial={version:{full: "1.1.21-master-0ab73bd"}};
