cd41c36d84a95a7d4a8879622a541105383da1a3
Michael Prentice <splaktar@gmail.com>
2020-07-22 19:57:19 +0000

    release: v1.1.24-master-a13722e

--- angular-material.js
+++ angular-material.js
@@ -2,7 +2,7 @@
  * AngularJS Material Design
  * https://github.com/angular/material
  * @license MIT
- * v1.1.24-master-f2fca2e
+ * v1.1.24-master-a13722e
  */
 (function( window, angular, undefined ){
 "use strict";
@@ -39220,4 +39220,4 @@ angular.module("material.core").constant("$MD_THEME_CSS", "md-autocomplete.md-TH
 })();
 
 
-})(window, window.angular);;window.ngMaterial={version:{full: "1.1.24-master-f2fca2e"}};
+})(window, window.angular);;window.ngMaterial={version:{full: "1.1.24-master-a13722e"}};
