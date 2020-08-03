9e5bc72ca14a3083a5c09560a3d6d541d565057c
Jeremy Elbourn <jelbourn@google.com>
2020-08-03 19:57:08 +0000

    release: v1.2.0-master-963c34f

--- angular-material.js
+++ angular-material.js
@@ -2,7 +2,7 @@
  * AngularJS Material Design
  * https://github.com/angular/material
  * @license MIT
- * v1.2.0
+ * v1.2.0-master-963c34f
  */
 (function( window, angular, undefined ){
 "use strict";
@@ -39070,4 +39070,4 @@ angular.module("material.core").constant("$MD_THEME_CSS", "md-autocomplete.md-TH
 })();
 
 
-})(window, window.angular);;window.ngMaterial={version:{full: "1.2.0"}};
+})(window, window.angular);;window.ngMaterial={version:{full: "1.2.0-master-963c34f"}};
