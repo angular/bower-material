7df2e57b9614612a1dfc84a063ca2d2fa6c684b3
Michael Prentice <splaktar@gmail.com>
2021-02-13 01:40:57 +0000

    release: v1.2.2-master-b6e6826

--- angular-material.js
+++ angular-material.js
@@ -2,7 +2,7 @@
  * AngularJS Material Design
  * https://github.com/angular/material
  * @license MIT
- * v1.2.2-master-bd0f787
+ * v1.2.2-master-b6e6826
  */
 (function( window, angular, undefined ){
 "use strict";
@@ -39648,4 +39648,4 @@ angular.module("material.core").constant("$MD_THEME_CSS", "md-autocomplete.md-TH
 })();
 
 
-})(window, window.angular);;window.ngMaterial={version:{full: "1.2.2-master-bd0f787"}};
+})(window, window.angular);;window.ngMaterial={version:{full: "1.2.2-master-b6e6826"}};
