bcf9c83e0bfb4af510bc744a3593e3796c8fd742
Ed Clement <ed@devintent.com>
2021-12-22 22:19:06 +0000

    release: v1.2.4-master-6ec3454

--- angular-material.js
+++ angular-material.js
@@ -2,7 +2,7 @@
  * AngularJS Material Design
  * https://github.com/angular/material
  * @license MIT
- * v1.2.4-master-4cb5de8
+ * v1.2.4-master-6ec3454
  */
 (function( window, angular, undefined ){
 "use strict";
@@ -39663,4 +39663,4 @@ angular.module("material.core").constant("$MD_THEME_CSS", "md-autocomplete.md-TH
 })();
 
 
-})(window, window.angular);;window.ngMaterial={version:{full: "1.2.4-master-4cb5de8"}};
+})(window, window.angular);;window.ngMaterial={version:{full: "1.2.4-master-6ec3454"}};
